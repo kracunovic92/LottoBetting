a0a1b3a2abf742d32b290d43075e8fe0f000885c
kracunovic <lazar.kracunovic92@gmail.com>
2023-08-18 13:20:03 +0200

    Provera2

--- app/src/main/java/com/example/lotto/StartModel.kt
+++ app/src/main/java/com/example/lotto/StartModel.kt
@@ -3,18 +3,26 @@ package com.example.lotto
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.lotto.api_calls.RetrofitClient
+import com.example.lotto.data.CompleteOffer
+import com.example.lotto.data.LottoOffer
+import com.example.lotto.data.OfferNextNHours
 import com.example.lotto.data.Offers
+import com.example.lotto.data.PriorityLottoOffer
 import kotlinx.coroutines.launch
 
 class StartModel : ViewModel() {
 
     var data by mutableStateOf(Offers())
-    var  _selected by mutableStateOf(0)
+    var  _selected by mutableStateOf(Options.FAST)
+
+
+
 
     private val _options = listOf(
         SelectionOption("Brza",true, Options.FAST),
@@ -37,11 +45,19 @@ class StartModel : ViewModel() {
         _options.find{it.option == selectionOption.option}?.selected = true
         val selected = _options.find{ it.selected }
         if(selected?.value == Options.FAST)
-            _selected = 1
+            _selected = Options.FAST
         else if(selected?.value == Options.DAILY)
-            _selected = 2
+            _selected = Options.DAILY
         else if(selected?.value == Options.FULL)
-            _selected = 3
+            _selected = Options.FULL
 
     }
+    inline fun <reified T> currentOffer(): MutableList<T> {
+        return when(_selected) {
+            Options.FULL -> data.completeOffer as MutableList<T>
+            Options.DAILY -> data.offerNextNHours as MutableList<T>
+            Options.FAST -> data.priorityLottoOffer as MutableList<T>
+        }
+    }
+
 }
--- app/src/main/java/com/example/lotto/StartScreen.kt
+++ app/src/main/java/com/example/lotto/StartScreen.kt
@@ -1,8 +1,5 @@
 package com.example.lotto
 
-import android.annotation.SuppressLint
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -15,27 +12,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import com.example.lotto.api_calls.RetrofitClient
-import com.example.lotto.ui.theme.PurpleGrey80
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.example.lotto.data.CompleteOffer
+import com.example.lotto.data.OfferNextNHours
+import com.example.lotto.data.PriorityLottoOffer
 
 
 @Composable
@@ -47,46 +34,58 @@ fun StartScreen(
         modifier = Modifier.fillMaxSize()
     ) {
         Column (modifier = Modifier.fillMaxSize()){
-            MiddleStartScreen(viewModel)
-            BottomStartScreen(viewModel)
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(600.dp)
+            ){
+                MiddleStartScreen(viewModel)
+            }
+            Column {
+                BottomStartScreen(viewModel)
+            }
 
         }
     }
-
-
-
 
 
 }
+
 @Composable
 fun MiddleStartScreen(viewModel: StartModel){
 
-    if (viewModel._selected == 1){
-        var data = viewModel.data.priorityLottoOffer
-        LazyColumn(){
-            items(data){
-                item ->
-                Text(text = item.toString())
+    val data = viewModel.currentOffer<Any>()
+
+    LazyColumn(
+
+    ){
+        items(data){
+        offer->
+            when(offer){
+                is PriorityLottoOffer -> {
+                    gameList(offer)
+                }
+                is OfferNextNHours ->{
+                    //TODO
+                }
+                is CompleteOffer -> {
+                    //TODO
+                }
+
             }
         }
 
-    }else if(viewModel._selected == 2){
-        var data = viewModel.data.offerNextNHours
-        LazyColumn(){
-            items(data){
-                    item ->
-                Text(text = item.toString())
-            }
-        }
-    }else if(viewModel._selected == 3){
-        var data = viewModel.data.completeOffer
-        LazyColumn(){
-            items(data){
-                    item ->
-                Text(text = item.toString())
-            }
-        }
     }
+
+
+}
+
+
+@Composable
+fun gameList(offer: Any) {
+
+
 }
 
 @Composable
@@ -99,7 +98,8 @@ fun BottomStartScreen(viewModel:StartModel){
         verticalAlignment = Alignment.CenterVertically
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             SingleSelectionList(
                 viewModel.options,
@@ -111,7 +111,9 @@ fun BottomStartScreen(viewModel:StartModel){
 
 @Composable
 fun SingleSelectionList(options: List<SelectionOption>, onOptionClicked: (SelectionOption) -> Unit) {
-    LazyRow (modifier = Modifier.fillMaxWidth()){
+    LazyRow (modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
         items(options) { option -> SingleSelectionCard(option, onOptionClicked) }
     }
 }
